0dedf46b9972df7e968418501c5377f7694d965f
yashraj010 <53484623+yashraj010@users.noreply.github.com>
2019-11-06 10:40:37 +0530

    assignments
    all assignment

---
+++ assignment 1/components/MainComponent.js
@@ -0,0 +1,134 @@
+import React, { Component } from "react";
+import Menu from "./MenuComponent";
+import { DISHES } from "../shared/dishes";
+import Dishdetail from "./DishdetailComponent";
+import { View, Platform } from "react-native";
+import {
+  createStackNavigator,
+  createAppContainer,
+  createDrawerNavigator
+} from "react-navigation";
+import { Icon } from "react-native-elements";
+import Home from "./HomeComponent";
+import About from "./AboutComponent";
+import Contact from "./ContactComponent";
+
+const MenuNavigator = createStackNavigator(
+  {
+    Menu: { screen: Menu },
+    Dishdetail: { screen: Dishdetail }
+  },
+  {
+    initialRouteName: "Menu",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff"
+      }
+    }
+  }
+);
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home }
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+        color: "#fff"
+      },
+      headerTintColor: "#fff"
+    }
+  }
+);
+const AboutNavigator = createStackNavigator(
+  {
+    AboutUs: { screen: About }
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+        color: "#fff"
+      },
+      headerTintColor: "#fff"
+    }
+  }
+);
+const ContactNavigator = createStackNavigator(
+  {
+    ContactUs: { screen: Contact }
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+        color: "#fff"
+      },
+      headerTintColor: "#fff"
+    })
+    }
+  }
+);
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeNavigator,
+      defaultNavigationOptions: {
+        title: "Home",
+        drawerLabel: "Home"
+      }
+    },
+    "About Us": {
+      screen: AboutNavigator,
+      defaultNavigationOptions: {
+        title: "About Us",
+        drawerLabel: "About Us"
+      }
+    },
+    Menu: {
+      screen: MenuNavigator,
+      defaultNavigationOptions: {
+        title: "Menu",
+        drawerLabel: "Menu"
+      }
+    },
+    "Contact Us": {
+      screen: ContactNavigator,
+      defaultNavigationOptions: {
+        title: "Contact Us",
+        drawerLabel: "Contact Us"
+      }
+    }
+  },
+  {
+    drawerBackgroundColor: "#AED6F1 "
+  }
+);
+const AppContainer = createAppContainer(MainNavigator);
+class Main extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          paddingTop: Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight
+        }}
+      >
+        <AppContainer />
+      </View>
+    );
+  }
+}
+export default Main;// JavaScript source code
---
+++ assignment 3/ActionCreator.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap'; 
+import React, { Component } from 'react';
+import { Breadcrumb, BreadcrumbItem, Button, Card, CardBody, CardImg, 
+    CardText, CardTitle, Col, Label, Modal, ModalBody, ModalHeader, Row } from 'reactstrap'; 
+import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form'
+
+const maxLength = (len) => (val) => !(val) || (val.length <= len)
+const minLength = (len) => (val) => (val) && (val.length >= len)
+
+class CommentForm extends Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false
+          };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
+    handleSubmit(values) {
+        console.log("Current State is: " + JSON.stringify(values));
+        alert("Current State is: " + JSON.stringify(values));
+        this.toggleModal();
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    render() {
+        return(
+            <>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+            <ModalBody>
+                <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                    <Row className="form-group">
+                        <Label htmlFor="rating" md={2}>Rating</Label>
+                        <Col md={10}>
+                            <Control.select model=".rating" id="rating" name="rating"
+                                className="form-control">
+                                <option>1</option>
+                                <option>2</option>
+                                <option selected>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </Control.select>
+                        </Col>
+                    </Row>                            
+                    <Row className="form-group">
+                        <Label htmlFor="author" md={2}>Your Name</Label>
+                        <Col md={10}>
+                            <Control.text model=".author" id="author" name="author"
+                                placeholder="Your Name"
+                                className="form-control"
+                                validators={{
+                                    minLength: minLength(3), maxLength: maxLength(15)
+                                }}
+                                />
+                            <Errors
+                                className="text-danger"
+                                model=".author"
+                                show="touched"
+                                messages={{
+                                    required: 'Required',
+                                    minLength: 'Must be greater than 2 characters',
+                                    maxLength: 'Must be 15 characters or less',
+                                }}
+                            />
+                        </Col>
+                    </Row>
+                    <Row className="form-group">
+                        <Label htmlFor="comment" md={2}>Comment</Label>
+                        <Col md={10}>
+                        <Control.textarea model=".comment" id="comment" name="comment"
+                            rows="6"
+                            className="form-control" />
+                        </Col>
+                    </Row>
+                    <Row className="form-group">
+                        <Col md={{size:10, offset:2}}>
+                            <Button type="submit" color="primary">
+                            Submit
+                            </Button>
+                        </Col>
+                    </Row>
+                </LocalForm>
+            </ModalBody>
+            </Modal>
+            <Button outline onClick={this.toggleModal}>
+                <span className="fa fa-pencil fa-lg"></span> Submit Comment
+            </Button>
+            </>
+        );
+    }
+}
+
+function RenderDish({dish}) {
+    return(
+@@ -34,6 +131,7 @@ function RenderComments({comments}) {
+                <ul className="list-unstyled">
+                    {commentListItems}    
+                </ul>
+                <CommentForm/>
+            </div>
+        );
+    }
+@@ -50,6 +148,7 @@ const DishDetail = (props) => {
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>// JavaScript source code
